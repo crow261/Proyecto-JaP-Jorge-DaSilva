cfd4a8916262a4b875ff90ce98147fbba2d88f32
crow261 <cuervo.jorge@gmail.com>
2020-08-16 13:09:02 -0300

    Obligatorio 1
    Obligatorio 1

--- js/login.js
+++ js/login.js
@@ -1,6 +1,52 @@
+
+function validar() {
+    var i = document.getElementById("user").value;
+    var contenido = "";
+    if (i == "" || i == null ) {
+        contenido += `
+            <p>Debe ingresa un usuario </p>
+            `
+        document.getElementById("errorU").innerHTML = contenido;
+        return false
+    }
+    var j = document.getElementById("pass").value;
+    var contenido2 = "";
+        if (j == "" || j == null ) {
+
+        contenido2 += `
+        <p>Debe ingresar una contraseña </p>
+        `
+        document.getElementById("errorP").innerHTML = contenido2;
+        return false
+    }
+
+}
+    
+
+function enviar() {
+    if (validar() == false) {
+        return validar();
+    } else {
+        return link()
+    }
+}
+
+function link() {
+    if (!validar()) {
+        return location.href ="index2.html"
+    }
+}
+
+
+
+
+
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
 
 });
--- js/products.js
+++ js/products.js
@@ -1,6 +1,45 @@
+
+
+
+var categoriesArray = [];
+
+function showProductList(obj) {
+
+let htmlContentToAppend = "";
+for( let i = 0; i < obj.length; i++) {
+    let products = obj[i];
+
+    htmlContentToAppend += `
+    <div class= "card">
+    <div class "card-details">
+        <h2 class= "card-head"> ${products.cost} ${products.currency} </h2>
+        <h1> ${products.name}</h1>
+        <p> ${products.description}
+        </br>
+        Cantidad de unidades Vendidas ${products.soldCount}
+        </p>
+        <img src= "${products.imgSrc}">
+            </div>
+    </div>
+    `
+    document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+}
+
+
+
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if(resultObj.status === "ok")
+        {
+        categoriesArray = resultObj.data;
+        
+        showProductList(categoriesArray);
+        }   
+    });
 
 });
