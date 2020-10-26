4a9bd1bb14604e6cd2eefe2fb2d693dd4c8b1d29
crow261 <cuervo.jorge@gmail.com>
2020-10-25 22:48:32 -0300

    ENTREGA 6

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,329 @@
+const CARRITO_DESAFIATE = "https://japdevdep.github.io/ecommerce-api/cart/654.json";
+let carrito;
+//variable para mensaje final
+var textoCortado;
+//variables de calculos
+var porcentaje = 0;
+var cantidadsubtotal = 0;
+var costodeporcentaje = 0;
+var subtotal = 0;
+var sumatotal = 0;
+//variable para alternar validación de pagos
+var formaPago = 0;
+//por defecto en dólares
+//var monedaActual = "USD";
+//var monedas = 1;
+
+
+
+
+//div con fila articulos, con for recorriendo el json de articulos, agregando fila del articulo y indice a las id
+function itemsCarrito(array) {
+
+    let htmlContentToAppend = "";
+
+    for (let i = 0; i < array.articles.length; i++) {
+        let productos = array.articles[i];
+
+        htmlContentToAppend +=
+            `<tr>
+            <th scope="row" id="articulo">`+ productos.name + ` </th>
+            <td id="imagen"> <img class="img-fluid img-thumbnail" src="`+ productos.src + `" alt="" width="50"</td>
+            <td id="moneda">`+ productos.currency + ` </td>
+            <td><span id="costo`+ [i] + `">` + productos.unitCost + `</span></td>
+            <td id="cantidad"><input type="number" class="form-control" id="selector`+ [i] + `" onchange="actualizar()" min="0" value= ` + productos.count + `> </td>
+            <td id="costoCant`+ [i] + `">` + 0 + `</td>
+         </tr>
+         `
+
+
+        document.getElementById("articulos").innerHTML = htmlContentToAppend;
+    }
+
+}
+
+//funcion calculo de subtotal a dolares(numericamente quedaba mejor que un número grande) y cantidad de articulos
+
+
+function result(carrito) {
+    let object = carrito.articles;
+    let costo0 = document.getElementById('costo0').textContent;
+    let costo1 = document.getElementById('costo1').textContent;
+    let cantidad0 = document.getElementById('selector0').value;
+    let cantidad1 = document.getElementById('selector1').value;
+    //cambio de moneda a dol 
+    if (object[0].currency == 'UYU') {
+        costo0 = object[0].unitCost / 40;
+    }
+    if (object[1].currency == 'UYU') {
+        costo1 = object[1].unitCost / 40;
+    }
+    //summa y escritura en el html 
+    let resultado0 = cantidad0 * costo0;
+    let resultado1 = cantidad1 * costo1;
+    subtotal = resultado0 + resultado1;
+    document.getElementById('costoCant0').innerHTML = `USD ` + resultado0;
+    document.getElementById('costoCant1').innerHTML = `USD ` + resultado1;
+    document.getElementById('subtotal').innerHTML = `USD ` + subtotal;
+    //este valor se imprime en la tabla del total 
+    document.getElementById('subtotal0').innerHTML = subtotal;
+
+}
+
+
+
+
+// PARTE GRUPAL: valida el campo de tipo de envio, en la tarea grupal fue con value 0,1,2. lo modifique para usarlo con un boleano, me costó bastante que quedara funcionando.
+function validarEnvio() {
+
+    var enviof = document.getElementsByName("envioform");
+    var validar_envio = false;
+
+    var i = 0;
+    while (i < enviof.length) {
+        if (enviof[i].checked) {
+            validar_envio = true;
+
+        }
+        i++;
+    }
+
+    //desplega textos de validacion
+    if (validar_envio == false) {
+        document.getElementById("errorEnvio").innerHTML = "Debe seleccionar un tipo de envío";
+        return validar_envio;
+    } else {
+        document.getElementById("errorEnvio").innerHTML = "";
+        $('#modal').modal('show');
+        return validar_envio;
+    }
+}
+/* grupal 
+while (validar_envio=false){
+    if (enviof.checked){
+        validar_envio=true;
+     }
+    
+}*/
+
+/*if (!validar_envio){
+    document.getElementById("errorEnvio").innerHTML= "Debe seleccionar un tipo de envío";
+    return validar_envio;
+} else {
+  document.getElementById("errorEnvio").innerHTML= "";
+  $('#modal').modal('show');
+    return validar_envio;
+}*/
+
+
+
+
+
+
+
+//calculo de costo de envio y escritura en la tabla
+
+function costoEnvio(porcentajevar) {
+    porcentaje = porcentajevar
+    document.getElementsByName("envioform").value = porcentajevar;
+
+    cantidadsubtotal = subtotal0.textContent;
+    costodeporcentaje = cantidadsubtotal * porcentaje / 100;
+    document.getElementById("envio").innerHTML = costodeporcentaje;
+}
+
+//suma del total
+function Total() {
+
+    sumatotal = parseFloat(cantidadsubtotal) + parseFloat(costodeporcentaje);
+    document.getElementById("total").innerHTML = sumatotal;
+}
+
+
+
+
+//cambio moneda a pesos, por defecto imprime en USD.
+
+function cambiodemoneda() {
+    monedas = document.getElementById("monedass").value;
+    if (monedas == 1) {
+        monedaActual = "UYU";
+        document.getElementById("cambio").innerHTML = monedaActual;
+        cambioPesos = 40;
+        costodeporcentajepesos = costodeporcentaje * cambioPesos;
+        sumatotalpesos = sumatotal * cambioPesos;
+        subtotalpesos = subtotal * cambioPesos;
+        document.getElementById("envio").innerHTML = costodeporcentajepesos;
+        document.getElementById("total").innerHTML = sumatotalpesos;
+        document.getElementById("subtotal0").innerHTML = subtotalpesos;
+    }
+    if (monedas == 0) {
+        monedaActual = "USD";
+        document.getElementById("cambio").innerHTML = monedaActual;
+        document.getElementById("envio").innerHTML = costodeporcentaje;
+        document.getElementById("total").innerHTML = sumatotal;
+        document.getElementById("subtotal0").innerHTML = subtotal;
+    }
+}
+
+
+
+
+
+
+
+
+//funcion de onchange para actualizar al tocar el input selector
+function actualizar() {
+    result(carrito);
+    costoEnvio(porcentaje);
+    Total();
+    cambiodemoneda();
+}
+
+
+
+
+
+
+//funciones para habilitar/desabilitar crédito/transferencia collapse
+function desplegarCredito() {
+    document.getElementById("tarjetas0").disabled = false;
+    document.getElementById("nombre0").disabled = false;
+    document.getElementById("numero0").disabled = false;
+    document.getElementById("cod0").disabled = false;
+
+
+    //transferencia
+    document.getElementById("tarjetas1").disabled = true;
+    document.getElementById("nombre1").disabled = true;
+    document.getElementById("numero1").disabled = true;
+    document.getElementById("cod1").disabled = true;
+    formaPago = 1;
+}
+
+function desplegarTransferencia() {
+    document.getElementById("tarjetas0").disabled = true;
+    document.getElementById("nombre0").disabled = true;
+    document.getElementById("numero0").disabled = true;
+    document.getElementById("cod0").disabled = true;
+
+    //transferencia
+    document.getElementById("tarjetas1").disabled = false;
+    document.getElementById("nombre1").disabled = false;
+    document.getElementById("numero1").disabled = false;
+    document.getElementById("cod1").disabled = false;
+    formaPago = 2;
+}
+
+
+
+
+//validaciones
+
+function validarMetodoPago() {
+    if (formaPago == 1) {
+        document.getElementById("errorPago").innerHTML = "";
+        var error = "";
+        tarjetas0 = document.getElementById("tarjetas0").value;
+        nombre0 = document.getElementById("nombre0").value;
+        numero0 = document.getElementById("numero0").value;
+        cod0 = document.getElementById("cod0").value;
+        if (tarjetas0 == "0" || nombre0 == "" || nombre0 == null || numero0 == "" || numero0 == null || cod0 == "" || cod0 == null) {
+            error += `
+            <h5>Debe llenar datos correctos de pago</h5>
+            `
+            document.getElementById("errorPago").innerHTML = error;
+            return false;
+        }
+
+    }
+    if (formaPago == 2) {
+        document.getElementById("errorPago").innerHTML = "";
+        var error = "";
+        tarjetas1 = document.getElementById("tarjetas1").value;
+        nombre1 = document.getElementById("nombre1").value;
+        numero1 = document.getElementById("numero1").value;
+        cod1 = document.getElementById("cod1").value;
+        if (tarjetas1 == "0" || nombre1 == "" || nombre1 == null || numero1 == "" || numero1 == null || cod1 == "" || cod1 == null) {
+            error += `
+        <h5>Debe llenar datos correctos de pago</h5>
+        `
+            document.getElementById("errorPago").innerHTML = error;
+            return false;
+        }
+
+    }
+    if (formaPago == 0) {
+        var error = "";
+        error += `
+        <h5>Debe seleccionar un método de pago</h5>
+        `
+        document.getElementById("errorPago").innerHTML = error;
+        return false;
+    }
+    return true;
+}
+
+
+
+//funcion que valida los datos de envio, todos los campos a la vez. basado en el logín hecho previo
+function validarDatosEnvio() {
+    document.getElementById("errorEnv").innerHTML = "";
+    var error = "";
+    direccion = document.getElementById("direccion").value;
+    pais = document.getElementById("pais").value;
+    departamento = document.getElementById("departamento").value;
+    localidad = document.getElementById("localidad").value;
+    if (direccion == "" || direccion == null || pais == "" || pais == null || departamento == "" || departamento == null || localidad == "" || localidad == null) {
+        error += `
+            <h5>Debe llenar todos los datos de envío</h5>
+            `
+        document.getElementById("errorEnv").innerHTML = error;
+        return false
+    }
+
+    return true;
+}
+
+//funcion que evalua los true/false de las validaciones y carga el texto del JSON
+function completarCompra() {
+
+    if (validarMetodoPago() == true && validarDatosEnvio() == true) {
+
+        document.getElementById("completo").innerHTML = textoCortado;
+
+    }
+}
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(CARRITO_DESAFIATE).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            carrito = resultObj.data;
+            itemsCarrito(resultObj.data);
+            result(carrito)
+
+
+        }
+    });
+
+    //llamar al JSON de compra exitosa
+    getJSONData(CART_BUY_URL).then(function (response) {
+        if (response.status === "ok") {
+            exito = response.data;
+            //pasar a texto el json y corto el inicio
+            let textoCompleto = JSON.stringify(exito);
+            textoCortado = textoCompleto.slice(8, 32);
+
+        }
+
+    });
+
+    cambiodemoneda();
 
 });
--- js/product-info.js
+++ js/product-info.js
@@ -1,6 +1,213 @@
+var product = [];
+var productList = [];
+var relatedNumber= [];
+
+//1 imagenes en carrousel
+
+function showProductImages(array) {
+
+   let htmlContentToAppend = "";
+   htmlContentToAppend += `
+
+<div>
+<div id="carouselExampleControls" class="carousel slide d-flex justify-content-center" data-ride="carousel" >
+<div class="carousel-inner">
+  <div class="carousel-item active">
+    <img class="d-block w-100" src="`+product.images[0]+`" alt="First slide">
+  </div>
+  <div class="carousel-item">
+    <img class="d-block w-100" src="`+product.images[1]+`" alt="Second slide">
+  </div>
+  <div class="carousel-item">
+    <img class="d-block w-100" src="`+product.images[2]+`" alt="Third slide">
+  </div>
+  <div class="carousel-item ">
+    <img class="d-block w-100" src="`+product.images[3]+`" alt="Third slide">
+  </div>
+  <div class="carousel-item">
+    <img class="d-block w-100" src="`+product.images[4]+`" alt="Third slide">
+  </div>
+  
+</div>
+<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+  <span class="sr-only">Previous</span>
+</a>
+<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+  <span class="sr-only">Next</span>
+</a>
+</div>
+</div>
+
+</div>
+
+        `
+
+        document.getElementById("productImagesGallery").innerHTML = htmlContentToAppend;
+    }
+
+
+
+
+
+
+
+
+
+
+
+//2 carga de comentarios
+
+function showProductComments(array) {
+    let htmlContentToAppend = ""; 
+
+    for (let j = 0; j < array.length; j++) {
+        let comments = array[j];
+        //estrellas iconos segun puntos
+        let puntos = comments.score;
+        estrellas = (`<span class="fa fa-star checked"></span>`).repeat(puntos);
+        estrellasnegras = (`<span class="fa fa-star"></span>`).repeat(5 - puntos);
+
+        htmlContentToAppend += `
+         <div class="container">    
+    <hr>
+    <div class="row comment">
+        <div class="head">
+            <small><strong class='user'>` + comments.user + `</strong>` + "  " + comments.dateTime + ` </small>
+        </div>   
+        <div class="row comment">
+        <div>   
+        `+ estrellas + `` + estrellasnegras + `
+        <div>
+        `+ comments.description + `
+        </div>
+        </div>
+        </div>
+    </div>
+    <hr>
+  </div>
+  `
+
+        document.getElementById("productComments").innerHTML = htmlContentToAppend;
+    }
+
+}
+
+
+
+
+//parte grupal
+function Comentario() {
+    //limpia el div de error al no comentar correctamente
+    document.getElementById("errorDiv").innerHTML = "";
+    let nuevoArreglo = [];
+    let comentario = "";
+    let puntuacion = "";
+    var today = new Date;
+    comentario = document.getElementById("nuevoComentario").value;
+    puntuacion = document.getElementById("punt").value;
+    //validacion del comentario
+    var error = "";
+    if (puntuacion == "0" || comentario == "") {
+        error += `<p style="color: red">Debe ingresa un comentario y una puntuación </p>
+            `
+        document.getElementById("errorDiv").innerHTML = error;
+        //probar comentario en un alert, porque no llegue a termina que lo agregue     
+    } else { alert(comentario + " " + puntuacion + " " + today); }
+}
+
+
+
+// funcion de mostrar productos relacionados
+
+function relatedProducts(array){  
+htmlContentToAppend="";
+/*for (let k = 0; k < array; k++) {
+  let relatedNumber = array[k];*/
+for(k in array){
+  let relatedNumber = array[k];
+ let prodPosition = productList[relatedNumber]
+
+htmlContentToAppend +=`
+<div class="col">`+prodPosition.name+`<img src="`+prodPosition.imgSrc+`" alt="..." class="img-thumbnail rounded" ></div> 
+`
+
+document.getElementById("related").innerHTML = htmlContentToAppend;
+}
+
+}
+
+
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCT_INFO_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+
+
+            product = resultObj.data;
+
+            let productNameHTML = document.getElementById("productName");
+            let productDescriptionHTML = document.getElementById("productDescription");
+            let productCosttHTML = document.getElementById("productCost");
+            let productCurrencyHTML = document.getElementById("productCurrency");
+          
+            productNameHTML.innerHTML = product.name;
+            productDescriptionHTML.innerHTML = product.description;
+            productCosttHTML.innerHTML = product.cost;
+            productCurrencyHTML.innerHTML = product.currency;
+            //numero de posición
+            relatedNumber = product.relatedProducts;
+
+
+            //Muestro las imagenes en forma de galería
+           showProductImages(product.images);
+           
+
+
+        }
+
+
+    });
+
+
+
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function (result) {
+        if (result.status === "ok") {
+            comments = result.data;
+
+            let commentsHTML = document.getElementById("commentsLength");
+            commentsHTML.innerHTML = comments.length;
+
+
+            //Muestro los comentarios
+            showProductComments(comments);
+        }
+
+
+
+    });
+    
+  
+
+   getJSONData(PRODUCTS_URL).then(function(response){
+        if(response.status === "ok"){
+          productList = response.data;
+          relatedProducts(product.relatedProducts);
+
+          
+        }
+
+        });
+
+
+
+
+    
 
 });
